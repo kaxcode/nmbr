228b3e700e61fe32659de51d997d35ba626d253f
Kenia Valladarez <keniavalladarez@gmail.com>
2016-11-15 14:03:25 -0500

    Assgined a variable to the input we get from user

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,9 +1,9 @@
-# create random number
+# create random number & save random number to variable
   random = rand(1..100)
   p random
 
-#save random number to variable
-
 #Prompt user for their #
+  print "Hello, please type a number from 1-100:"
+  users_number = gets.chomp
 
 #Compare user input to the variable
