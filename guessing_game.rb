871bd69a3edac60f27a3c2f888aeb3820a59fffa
Kenia Valladarez <keniavalladarez@gmail.com>
2016-11-15 15:19:35 -0500

    Created the break to stop program once the user tries 5 times and failes

--- guessing_game.rb
+++ guessing_game.rb
@@ -7,13 +7,36 @@
   winning_number = computer_number.to_i
   p winning_number
 
-# get usere number
+# get users number
 
   def users_input(question)
     print "#{question}"
     gets.chomp
   end
 
-user_answer = users_input("What is your ramdom number?")
-users_number = user_answer.to_i
-p users_number
+count=0
+
+loop do
+  count = count +1
+  user_answer = users_input("What is your ramdom number?")
+  users_number = user_answer.to_i
+  p users_number
+
+  if users_number >= winning_number
+    puts "TOO HIGH, please try again"
+  end
+
+  if users_number <= winning_number
+    puts "TOO LOW, please try again"
+  end
+
+  if users_number == winning_number
+    puts "You got it!!!"
+    break
+  end
+
+  if count >= 5
+    puts "You loooose!"
+    break
+  end
+end
