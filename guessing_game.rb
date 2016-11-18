08e313a424756168b182439db558ecc19514715b
Kenia Valladarez <keniavalladarez@gmail.com>
2016-11-18 09:53:17 -0500

    Fixed rubocop linter messages

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,50 +1,49 @@
-# create random number & save random number to variable
+  # create random number & save random number to variable
   def computer_random(number)
     return "#{number}"
   end
 
-  computer_number = computer_random(rand(1..100))Add
+  computer_number = computer_random(rand(1..100))
   winning_number = computer_number.to_i
   # CHEAT! GET THE WINNING NUMBER
   # p winning_number
 
-# get users number
-
+  # get users number
   def users_input(question)
     print "#{question}"
     gets.chomp
   end
 
-count=0
-guesses= []
+  count = 0
+  guesses = []
 
-loop do
-  user_answer = users_input("What is your ramdom number?🤔   ")
-  users_number = user_answer.to_i
+  loop do
+    user_answer = users_input("What is your ramdom number?🤔   ")
+    users_number = user_answer.to_i
 
-  if guesses.include?(users_number)
-    puts "Are you sleepy? You tried that number already"
+    if guesses.include?(users_number)
+      puts "Are you sleepy? You tried that number already"
+    end
+
+    guesses << users_number
+
+    if users_number >= winning_number
+      puts "#{users_number} is TOO HIGH 👆, please try again"
+    end
+
+    if users_number <= winning_number
+      puts "TOO LOW 👇, please try again"
+    end
+
+    if users_number == winning_number
+      puts "You got it!!! 👏🍕🍾🚀🎉"
+      break
+    end
+
+    count += count
+
+    if count >= 5
+      puts "You loooose! 😵 "
+      break
+    end
   end
-
-  guesses << users_number
-
-  if users_number >= winning_number
-    puts "#{users_number} is TOO HIGH 👆, please try again"
-  end
-
-  if users_number <= winning_number
-    puts "TOO LOW 👇, please try again"
-  end
-
-  if users_number == winning_number
-    puts "You got it!!! 👏🍕🍾🚀🎉"
-    break
-  end
-
-  count = count +1
-
-  if count >= 5
-    puts "You loooose! 😵 "
-    break
-  end
-end
